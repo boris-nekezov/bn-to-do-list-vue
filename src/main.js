9632264e3b56332511389f3394c2080611116cc5
Boris Nekezov <boris.nekezov@gmail.com>
2020-03-16 12:13:49 +0200

    Fresh install without prettier instead prevention only

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import { rtdbPlugin } from "vuefire";
 
-// bootstrap slate
-import "bootswatch/dist/slate/bootstrap.min.css";
-
 // font-awesome inlcuded in index.html
 // https://fontawesome.com/v4.7.0/icons/
 
